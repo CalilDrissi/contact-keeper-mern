b12c939c5efa8c8ad7313e99b03661012d94b79a
Calil Drissi <calildrissi@vivaldi.net>
2021-07-27 11:29:55 +0100

    initialisation

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const connectDB = require('./config/db');
+const path = require('path');
+const app = express();
+
+// db connection
+connectDB();
+
+// init middleware
+app.use(express.json({extend: false}));
+
+const PORT = process.env.PORT || 8000;
+
+// Define Routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
+
+// Serve static assets in production
+if(process.env.NODE_ENV  === 'production') {
+    //set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req,res)=>res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
+
+
+
+
+
+app.listen(PORT, ()=>{
+    console.log(`Server started on port ${PORT}`)
+})
+
+
+
